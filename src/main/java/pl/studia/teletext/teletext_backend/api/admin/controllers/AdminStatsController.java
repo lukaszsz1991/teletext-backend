40c8ae3d92f1341ad94b8a7339d18f8555cbd4ee
Sebastian Górski <134208901+sgorski00@users.noreply.github.com>
2025-10-24 09:15:19 +0200

    chore(api-docs): add swagger (#15)
    Dodano:
    - potrzebne zależności
    - konfigurację metadanych w OpenAPI
    - ogólną konfigurację Swaggera
    - dokumentację enpointów /api/admin/**

--- src/main/java/pl/studia/teletext/teletext_backend/api/admin/controllers/AdminStatsController.java
+++ src/main/java/pl/studia/teletext/teletext_backend/api/admin/controllers/AdminStatsController.java
@@ -1,24 +1,31 @@
 package pl.studia.teletext.teletext_backend.api.admin.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.studia.teletext.teletext_backend.api.admin.dtos.stats.TeletextPageStatsResponse;
 import pl.studia.teletext.teletext_backend.domain.services.TeletextPageStatsService;
 
 @RestController
 @RequestMapping("/api/admin/stats")
 @RequiredArgsConstructor
+@Tag(name = "Pages stats viewer controller", description = "Operations for Teletext page stats.")
 public class AdminStatsController {
 
   private final TeletextPageStatsService teletextPageStatsService;
 
   //TODO: add all pages stats sorted by views desc
 
+  @Operation(summary = "Get all views of page", description = "Returns counted views with details.")
+  @ApiResponse(responseCode = "200", description = "Page found, stats returned successfully. Can return empty list if no views.")
   @GetMapping("pages/{pageNumber}")
-  public ResponseEntity<?> getPageStats(
+  public ResponseEntity<TeletextPageStatsResponse> getPageStats(
     @PathVariable Integer pageNumber
   ) {
     var page = teletextPageStatsService.getStatsForPage(pageNumber);
