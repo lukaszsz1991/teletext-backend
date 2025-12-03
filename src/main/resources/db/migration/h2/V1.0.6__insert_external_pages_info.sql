09a20dec2771e1d65e0283bac8e214bb2185b7ec
Sebastian Górski <sebastiangorski00@gmail.com>
2025-12-03 08:32:54 +0100

    chore: cleanup migrations

--- src/main/resources/db/migration/h2/V1.0.6__insert_external_pages_info.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO pages(page_number, category, template_id)
-SELECT 501, 'WEATHER', id FROM page_templates WHERE name = 'Pogoda Wrocław';
-
-INSERT INTO pages(page_number, category, template_id)
-SELECT 201, 'SPORTS', id FROM page_templates WHERE name = 'Tabela Ekstraklasa';
--- src/main/resources/db/migration/postgresql/V1.0.7__alter_contents_table.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE page_contents ADD COLUMN source VARCHAR(255);
-ALTER TABLE page_contents ADD COLUMN additional_data JSONB;
