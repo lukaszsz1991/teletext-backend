1accf4213e404b25db5212c2003804b6b1eeb178
Sebastian Górski <sebastiangorski00@gmail.com>
2025-12-08 11:55:14 +0100

    fix: change jwt secret env to dynamic generated value + lint

--- src/main/java/pl/studia/teletext/teletext_backend/config/SecurityConfiguration.java
+++ src/main/java/pl/studia/teletext/teletext_backend/config/SecurityConfiguration.java
@@ -35,7 +35,11 @@ public class SecurityConfiguration {
             request ->
                 request
                     .requestMatchers(
-                        "/api/public/**", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/actuator/health")
+                        "/api/public/**",
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/actuator/health")
                     .permitAll()
                     .requestMatchers("/api/admin/auth/login")
                     .permitAll()
