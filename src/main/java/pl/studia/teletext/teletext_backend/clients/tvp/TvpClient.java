e0e965fe35e78bd26ddae832f83337fad20fcfb3
Sebastian Górski <sebastiangorski00@gmail.com>
2025-12-22 10:49:00 +0100

    linter

--- src/main/java/pl/studia/teletext/teletext_backend/clients/tvp/TvpClient.java
+++ src/main/java/pl/studia/teletext/teletext_backend/clients/tvp/TvpClient.java
@@ -1,5 +1,7 @@
 package pl.studia.teletext.teletext_backend.clients.tvp;
 
+import java.net.URI;
+import java.time.LocalDate;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatusCode;
@@ -10,13 +12,10 @@ import org.springframework.web.util.UriComponentsBuilder;
 import pl.studia.teletext.teletext_backend.exceptions.ExternalApiException;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
-import java.time.LocalDate;
-
 @Log4j2
 @Component
 public class TvpClient {
-  private final static String BASE_PREFIX = "prasa";
+  private static final String BASE_PREFIX = "prasa";
   private final WebClient tvpWebClient;
 
   public TvpClient(@Qualifier("tvpWebClient") WebClient tvpWebClient) {
@@ -25,42 +24,46 @@ public class TvpClient {
 
   public Mono<TvpResponse> fetchTvProgram(TvpChannel channel, LocalDate date) {
     return tvpWebClient
-      .get()
-      .uri(uri -> buildUri(channel, date))
-      .retrieve()
-      .onStatus(
-        HttpStatusCode::isError,
-        TvpClient::handleError)
-      .bodyToMono(TvpResponse.class);
+        .get()
+        .uri(uri -> buildUri(channel, date))
+        .retrieve()
+        .onStatus(HttpStatusCode::isError, TvpClient::handleError)
+        .bodyToMono(TvpResponse.class);
   }
 
   private static Mono<Throwable> handleError(ClientResponse response) {
-    return response.bodyToMono(String.class)
-      .flatMap(errorBody -> {
-        log.error("Error fetching data from TVP: {}", errorBody);
-        return Mono.error(new ExternalApiException(
-          "Błąd pobierania danych programu TV z TVP", response.statusCode().value()));
-      });
+    return response
+        .bodyToMono(String.class)
+        .flatMap(
+            errorBody -> {
+              log.error("Error fetching data from TVP: {}", errorBody);
+              return Mono.error(
+                  new ExternalApiException(
+                      "Błąd pobierania danych programu TV z TVP", response.statusCode().value()));
+            });
   }
 
   private URI buildUri(TvpChannel channel, LocalDate date) {
     return UriComponentsBuilder.newInstance()
-      .pathSegment(BASE_PREFIX)
-      .pathSegment(channel.getUrlName())
-      .pathSegment(String.valueOf(date.getYear()))
-      .pathSegment("xml_OMI")
-      .pathSegment(buildXmlFileName(channel.getUrlCode(), date))
-      .build()
-      .toUri();
+        .pathSegment(BASE_PREFIX)
+        .pathSegment(channel.getUrlName())
+        .pathSegment(String.valueOf(date.getYear()))
+        .pathSegment("xml_OMI")
+        .pathSegment(buildXmlFileName(channel.getUrlCode(), date))
+        .build()
+        .toUri();
   }
 
   private String buildXmlFileName(String channelCode, LocalDate date) {
-    var fileName = String.format("m%4d%02d%02d_%s.xml",
-      date.getYear(),
-      date.getMonthValue(),
-      date.getDayOfMonth(),
-      channelCode);
-    log.debug("Built TVP XML file name: {}. File built with code {} and date {}.", fileName, channelCode, date);
+    var fileName =
+        String.format(
+            "m%4d%02d%02d_%s.xml",
+            date.getYear(), date.getMonthValue(), date.getDayOfMonth(), channelCode);
+    log.debug(
+        "Built TVP XML file name: {}. File built with code {} and date {}.",
+        fileName,
+        channelCode,
+        date);
     return fileName;
   }
 }
--- src/main/java/pl/studia/teletext/teletext_backend/clients/tvp/TvpResponse.java
+++ src/main/java/pl/studia/teletext/teletext_backend/clients/tvp/TvpResponse.java
@@ -1,26 +1,18 @@
 package pl.studia.teletext.teletext_backend.clients.tvp;
 
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonRootName;
+import java.time.LocalDateTime;
 import tools.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import tools.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
-import java.time.LocalDateTime;
-
 @JsonRootName("APCData")
 public record TvpResponse(
-  @JacksonXmlElementWrapper(useWrapping = false)
-  @JacksonXmlProperty(localName = "prrecord")
-  PrRecord[] records
-) {
+    @JacksonXmlElementWrapper(useWrapping = false) @JacksonXmlProperty(localName = "prrecord")
+        PrRecord[] records) {
   public record PrRecord(
-    @JacksonXmlProperty(localName = "REAL_DATE_TIME")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    LocalDateTime realDateTime,
-    @JacksonXmlProperty(localName = "PTITEL")
-    String title,
-    @JacksonXmlProperty(localName = "EPG")
-    String description
-  ) {}
+      @JacksonXmlProperty(localName = "REAL_DATE_TIME") @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+          LocalDateTime realDateTime,
+      @JacksonXmlProperty(localName = "PTITEL") String title,
+      @JacksonXmlProperty(localName = "EPG") String description) {}
 }
