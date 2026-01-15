58b8e94ed35d60ff33097f41326da3d405a9280f
sgorski00 <sebastiangorski00@gmail.com>
2026-01-15 17:31:44 +0100

    migration fix

--- src/main/resources/db/migration/h2/V1.1.0__insert_test_data.sql
+++ src/main/resources/db/migration/h2/V1.1.0__insert_test_data.sql
@@ -3,8 +3,14 @@ SELECT id FROM pages WHERE page_number = 101
 UNION ALL
 SELECT id FROM pages WHERE page_number = 101;
 
-INSERT INTO pages (page_number, category)
-VALUES
-  (801, 'sports'),
-  (802, 'finance'),
-  (803, 'weather');
+INSERT INTO page_templates (name, category, source, config_json)
+VALUES ('Szablon Sportowy 1', 'SPORTS', 'SPORT_TABLE', '{"league": "bundesliga"}'),
+       ('Szablon Finansowy 1', 'FINANCE', 'EXCHANGE_RATE', '{"currencyCode": "USD"}'),
+       ('Szablon Pogodowy 1', 'WEATHER', 'WEATHER', '{"city": "Warszawa"}');
+
+INSERT INTO pages (page_number, category, template_id)
+SELECT 299, 'SPORTS',   id FROM page_templates WHERE name = 'Szablon Sportowy 1'
+UNION ALL
+SELECT 899, 'FINANCE',  id FROM page_templates WHERE name = 'Szablon Finansowy 1'
+UNION ALL
+SELECT 599, 'WEATHER',  id FROM page_templates WHERE name = 'Szablon Pogodowy 1';
